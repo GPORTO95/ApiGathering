7f156980ca999537aa4dc9dba2a5e63082f12113
Gabriel Porto <gr_porto@hotmail.com>
2023-06-16 22:32:23 -0300

    add interfaces base CQRS and controller base

---
+++ src/Gatherly.Presentation/Controllers/MembersController.cs
@@ -0,0 +1,40 @@
+﻿using Gatherly.Application.Members.Commands.CreateMember;
+using Gatherly.Domain.Shared;
+using Gatherly.Presentation.Abstractions;
+using Gatherly.Presentation.Contracts.Members;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gatherly.Presentation.Controllers;
+
+[Route("api/members")]
+public sealed class MembersController : ApiController
+{
+    public MembersController(ISender sender) 
+        : base(sender)
+    { }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetMemberId(Guid id, CancellationToken cancellationToken)
+    {
+        //var query = new 
+
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterMember(
+        [FromBody] RegisterMemberRequest request,
+        CancellationToken cancellationToken)
+    {
+        //var command = new CreateMemberCommand(
+        //    request.Email,
+        //    request.FirstName,
+        //    request.LastName);
+
+        //Result<Guid> result = await Sender.Send(command, cancellationToken);
+
+        return Ok();
+    }
+
+}
