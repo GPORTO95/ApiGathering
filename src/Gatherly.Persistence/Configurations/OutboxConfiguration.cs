5ca9c4e1027470ff591c8cbc1f78e2f2089fda16
Gabriel Porto <gr_porto@hotmail.com>
2023-06-18 23:38:07 -0300

    Add class Persistence

---
+++ src/Gatherly.Persistence/Configurations/OutboxConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Gatherly.Domain.Entities;
+using Gatherly.Persistence.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Gatherly.Persistence.Configurations;
+
+internal sealed class OutboxConfiguration : IEntityTypeConfiguration<Invitation>
+{
+    public void Configure(EntityTypeBuilder<Invitation> builder)
+    {
+        builder.ToTable(TableNames.OutboxMessages);
+
+        builder.HasKey(x => x.Id);
+    }
+}
---
+++ src/Gatherly.Persistence/Constants/TableNames.cs
@@ -0,0 +1,15 @@
+﻿namespace Gatherly.Persistence.Constants;
+
+internal sealed class TableNames
+{
+    internal const string Members = nameof(Members);
+
+    internal const string Gatherings = nameof(Gatherings);
+
+    internal const string Invitations = nameof(Invitations);
+
+    internal const string Attendess = nameof(Attendess);
+
+    internal const string OutboxMessages = nameof(OutboxMessages);
+
+}
