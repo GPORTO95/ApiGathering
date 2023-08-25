107c964c98c6ab87768a9259b01d18c3f5f6ee48
Gabriel Porto <gr_porto@hotmail.com>
2023-08-24 22:22:27 -0300

    Estruturando Injeção de Dependência da maneira correta

---
+++ src/Gatherly.App/Configuration/InfrastructureServiceInstaller.cs
@@ -0,0 +1,38 @@
+﻿using Gatherly.Persistence.Interceptors;
+using Gatherly.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Scrutor;
+using Gatherly.App.Middlewares;
+
+namespace Gatherly.App.Configuration;
+
+public class InfrastructureServiceInstaller : IServiceInstaller
+{
+    public void Install(IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .Scan(
+                selector => selector
+                    .FromAssemblies(
+                        Gatherly.Infrastructure.AssemblyReference.Assembly,
+                        Gatherly.Persistence.AssemblyReference.Assembly)
+                    .AddClasses(false)
+                    .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+                    .AsMatchingInterface()
+                    .WithScopedLifetime());
+
+        services.AddSingleton<ConvertDomainEventsToOutboxMessagesInterceptor>();
+
+        services.AddSingleton<UpdateAuditableEntitiesInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>(
+            (sp, optionsBuilder) =>
+            {
+                optionsBuilder.UseSqlServer(
+                    configuration.GetConnectionString("Database"));
+            });
+
+        services.AddLogging();
+        services.AddTransient<GlobalExceptionHandlingMiddleware>();
+    }
+}
